d5fc40a0f12c56058030d7bbcb001782b305c1f4
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-18 20:04:52 +0800

    init instance

---
+++ README.md
@@ -0,0 +1,166 @@
+# `wl-gammarelay-rs`
+
+Like [`wl-gammarelay`](https://github.com/jeremija/wl-gammarelay), but written in rust, runs on a single thread and has zero runtime dependencies.
+
+## Dbus interface
+
+```
+$ busctl --user introspect rs.wl-gammarelay / rs.wl.gammarelay
+NAME               TYPE      SIGNATURE RESULT/VALUE FLAGS
+.ToggleInverted    method    -         -            -
+.UpdateBrightness  method    d         -            -
+.UpdateGamma       method    d         -            -
+.UpdateTemperature method    n         -            -
+.Brightness        property  d         1            emits-change writable
+.Gamma             property  d         1            emits-change writable
+.Inverted          property  b         false        emits-change writable
+.Temperature       property  q         6500         emits-change writable
+```
+
+## Installation
+
+[![Packaging status](https://repology.org/badge/vertical-allrepos/wl-gammarelay-rs.svg)](https://repology.org/project/wl-gammarelay-rs/versions)
+
+### From Source
+
+```sh
+cargo install wl-gammarelay-rs --locked
+```
+
+## Status bar integration
+
+### i3status-rust
+
+i3status-rust hueshift block has the builtin support for this backend since 0.21.6.
+
+### Waybar
+
+```json
+    "custom/wl-gammarelay-temperature": {
+        "format": "{} ",
+        "exec": "wl-gammarelay-rs watch {t}",
+        "on-scroll-up": "busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n +100",
+        "on-scroll-down": "busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n -100"
+    },
+    "custom/wl-gammarelay-brightness": {
+        "format": "{}% ",
+        "exec": "wl-gammarelay-rs watch {bp}",
+        "on-scroll-up": "busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateBrightness d +0.02",
+        "on-scroll-down": "busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateBrightness d -0.02"
+    }
+    "custom/wl-gammarelay-gamma": {
+        "format": "{}% γ",
+        "exec": "wl-gammarelay-rs watch {g}",
+        "on-scroll-up": "busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateGamma d +0.02",
+        "on-scroll-down": "busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateGamma d -0.02"
+    }
+```
+
+Note: make sure `wl-gammarelay-rs` is in your `PATH` at the time `waybar` is launched, or use the full path to the binary.
+
+### wl-gammarelay-applet
+
+[wl-gammarelay-applet](https://github.com/junelva/wl-gammarelay-applet) is a small desktop applet for controlling wl-gammarelay-rs.
+
+## Watch for changes
+
+You can run `wl-gammarelay-rs watch <format>` to watch for changes. Each time some property changes, a new line will be printed based on <format> string. `{t}` expands into temperature, `{b}` into brightness (0 to 1) and `{bp}` expands into brightness in percents (0 to 100).
+
+For example, if you want to monitor both temperature and brightness (in percents), you can run
+```sh
+$ wl-gammarelay-rs watch "{t}K {bp}%"
+4000K 90%
+4500K 90%
+5000K 90%
+5000K 100%
+5000K 80%
+5000K 100%
+...
+```
+
+## Example usage in scripts
+
+```sh
+# Set the temperature to `5000`
+busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 5000
+
+# Increase the temperature by `100`:
+busctl --user call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n 100
+
+# Decrease the temperature by `100`:
+busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n -100
+
+# Invert colors
+busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Inverted b true
+
+# Toggle inverted colors
+busctl --user call rs.wl-gammarelay / rs.wl.gammarelay ToggleInverted
+
+# Set the brightness to `100%`:
+busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Brightness d 1
+
+# Increase the brightness by `10%`:
+busctl --user call rs.wl-gammarelay / rs.wl.gammarelay UpdateBrightness d 0.1
+
+# Decrease the brightness by `10%`:
+busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateBrightness d -0.1
+
+# Set display gamma to `1.0`:
+busctl --user set-property rs.wl-gammarelay / rs.wl.gammarelay Gamma d 1
+
+# Increase gamma by `0.1`:
+busctl --user call rs.wl-gammarelay / rs.wl.gammarelay UpdateGamma d 0.1
+
+# Decrease gamma by `0.1`:
+busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateGamma d -0.1
+```
+
+## With multiple outputs
+
+Each connected output is listed under `/outputs` and its properties can be seen and edited separately. For example, a laptop with an internal "eDP-1" monitor and a "HDMI-A-1" output has the following DBus objects:
+
+```sh
+$ busctl --user tree rs.wl-gammarelay
+└─ /outputs
+  ├─ /outputs/HDMI_A_1
+  └─ /outputs/eDP_1
+```
+
+You can operate on a specific output using its object path:
+
+```sh
+# Set the temperature to `5000` for the HDMI output
+busctl --user set-property rs.wl-gammarelay /outputs/HDMI_A_1 rs.wl.gammarelay Temperature q 5000
+
+# Set the temperature to `9000` for the internal monitor
+busctl --user set-property rs.wl-gammarelay /outputs/eDP_1 rs.wl.gammarelay Temperature q 9000
+```
+
+When there are several outputs, the values shown are:
+
+- for the brightness, temperature and gamma, the average of all outputs' values
+- for the inverted boolean, true if all outputs are inverted and false otherwise
+
+When updating the brightness, temperature or gamma value, the modification is applied to each output:
+
+```sh
+# Get the values
+$ busctl --user -- get-property rs.wl-gammarelay /outputs/eDP_1 rs.wl.gammarelay Brightness
+d 0.7
+$ busctl --user -- get-property rs.wl-gammarelay /outputs/HDMI_A_1 rs.wl.gammarelay Brightness
+d 0.5
+
+# Update all outputs
+$ busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateBrightness d 0.05
+
+# Get the values again
+$ busctl --user -- get-property rs.wl-gammarelay /outputs/eDP_1 rs.wl.gammarelay Brightness
+d 0.75
+$ busctl --user -- get-property rs.wl-gammarelay /outputs/HDMI_A_1 rs.wl.gammarelay Brightness
+d 0.55
+```
+
+When toggling the inverted status:
+
+- if all the monitors are inverted, then all of them are reverted to normal
+- otherwise, all monitors are made inverted, even if they already were inverted
---
+++ src/wayland.rs
@@ -0,0 +1,212 @@
+use std::io::ErrorKind;
+use std::os::fd::{AsRawFd, RawFd};
+
+use anyhow::Result;
+
+use wayrs_client::cstr;
+use wayrs_client::global::*;
+use wayrs_client::protocol::*;
+use wayrs_client::{Connection, EventCtx, IoMode};
+use wayrs_protocols::wlr_gamma_control_unstable_v1::*;
+
+use crate::color::{colorramp_fill, Color};
+use crate::{State, WaylandState};
+
+pub struct Wayland {
+    conn: Connection<State>,
+}
+
+impl AsRawFd for Wayland {
+    fn as_raw_fd(&self) -> RawFd {
+        self.conn.as_raw_fd()
+    }
+}
+
+impl Wayland {
+    pub fn new() -> Result<(Self, WaylandState)> {
+        let (mut conn, globals) = Connection::connect_and_collect_globals()?;
+        conn.add_registry_cb(wl_registry_cb);
+
+        let gamma_manager = globals.bind(&mut conn, 1)?;
+
+        let outputs = globals
+            .iter()
+            .filter(|g| g.is::<WlOutput>())
+            .map(|output| Output::bind(&mut conn, output, gamma_manager))
+            .collect();
+
+        let state = WaylandState {
+            outputs,
+            gamma_manager,
+        };
+
+        conn.flush(IoMode::Blocking)?;
+
+        Ok((Self { conn }, state))
+    }
+
+    pub fn poll(&mut self, state: &mut State) -> Result<()> {
+        match self.conn.recv_events(IoMode::NonBlocking) {
+            Ok(()) => self.conn.dispatch_events(state),
+            Err(e) if e.kind() == ErrorKind::WouldBlock => (),
+            Err(e) => return Err(e.into()),
+        }
+
+        for output in &mut state.wayland_state.outputs {
+            if output.color_changed {
+                output.update_displayed_color(&mut self.conn)?;
+            }
+        }
+        self.conn.flush(IoMode::Blocking)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub struct Output {
+    reg_name: u32,
+    wl: WlOutput,
+    name: Option<String>,
+    color: Color,
+    gamma_control: ZwlrGammaControlV1,
+    ramp_size: usize,
+    color_changed: bool,
+}
+
+impl Output {
+    fn bind(
+        conn: &mut Connection<State>,
+        global: &Global,
+        gamma_manager: ZwlrGammaControlManagerV1,
+    ) -> Self {
+        eprintln!("New output: {}", global.name);
+        let output = global.bind_with_cb(conn, 4, wl_output_cb).unwrap();
+        Self {
+            reg_name: global.name,
+            wl: output,
+            name: None,
+            color: Color::default(),
+            gamma_control: gamma_manager.get_gamma_control_with_cb(conn, output, gamma_control_cb),
+            ramp_size: 0,
+            color_changed: true,
+        }
+    }
+
+    fn destroy(self, conn: &mut Connection<State>) {
+        eprintln!("Output {} removed", self.reg_name);
+        self.gamma_control.destroy(conn);
+        self.wl.release(conn);
+    }
+
+    pub fn reg_name(&self) -> u32 {
+        self.reg_name
+    }
+
+    pub fn color(&self) -> Color {
+        self.color
+    }
+
+    pub fn color_changed(&self) -> bool {
+        self.color_changed
+    }
+
+    pub fn set_color(&mut self, color: Color) {
+        if color != self.color {
+            self.color = color;
+            self.color_changed = true;
+        }
+    }
+
+    pub fn object_path(&self) -> Option<String> {
+        self.name
+            .as_deref()
+            .map(|name| format!("/outputs/{}", name.replace('-', "_")))
+    }
+
+    fn update_displayed_color(&mut self, conn: &mut Connection<State>) -> Result<()> {
+        if self.ramp_size == 0 {
+            return Ok(());
+        }
+
+        let file = shmemfdrs2::create_shmem(cstr!("/ramp-buffer"))?;
+        file.set_len(self.ramp_size as u64 * 6)?;
+        let mut mmap = unsafe { memmap2::MmapMut::map_mut(&file)? };
+        let buf = bytemuck::cast_slice_mut::<u8, u16>(&mut mmap);
+        let (r, rest) = buf.split_at_mut(self.ramp_size);
+        let (g, b) = rest.split_at_mut(self.ramp_size);
+        colorramp_fill(r, g, b, self.ramp_size, self.color);
+        self.gamma_control.set_gamma(conn, file.into());
+
+        self.color_changed = false;
+        Ok(())
+    }
+}
+
+fn wl_registry_cb(conn: &mut Connection<State>, state: &mut State, event: &wl_registry::Event) {
+    match event {
+        wl_registry::Event::Global(global) if global.is::<WlOutput>() => {
+            let mut output = Output::bind(conn, global, state.wayland_state.gamma_manager);
+            output.set_color(state.wayland_state.color());
+            output.update_displayed_color(conn).unwrap();
+            state.wayland_state.outputs.push(output);
+        }
+        wl_registry::Event::GlobalRemove(name) => {
+            if let Some(output_index) = state
+                .wayland_state
+                .outputs
+                .iter()
+                .position(|o| o.reg_name == *name)
+            {
+                if let Some(output_name) = &state.wayland_state.outputs[output_index].name {
+                    state.dbus_server.remove_output(output_name);
+                }
+                let output = state.wayland_state.outputs.swap_remove(output_index);
+                output.destroy(conn);
+            }
+        }
+        _ => (),
+    }
+}
+
+fn gamma_control_cb(ctx: EventCtx<State, ZwlrGammaControlV1>) {
+    let output_index = ctx
+        .state
+        .wayland_state
+        .outputs
+        .iter()
+        .position(|o| o.gamma_control == ctx.proxy)
+        .expect("Received event for unknown output");
+    match ctx.event {
+        zwlr_gamma_control_v1::Event::GammaSize(size) => {
+            let output = &mut ctx.state.wayland_state.outputs[output_index];
+            eprintln!("Output {}: ramp_size = {}", output.reg_name, size);
+            output.ramp_size = size as usize;
+            output.update_displayed_color(ctx.conn).unwrap();
+        }
+        zwlr_gamma_control_v1::Event::Failed => {
+            let output = ctx.state.wayland_state.outputs.swap_remove(output_index);
+            eprintln!("Output {}: gamma_control::Event::Failed", output.reg_name);
+            if let Some(output_name) = &output.name {
+                ctx.state.dbus_server.remove_output(output_name);
+            }
+            output.destroy(ctx.conn);
+        }
+        _ => (),
+    }
+}
+
+fn wl_output_cb(ctx: EventCtx<State, WlOutput>) {
+    if let wl_output::Event::Name(name) = ctx.event {
+        let output = ctx
+            .state
+            .wayland_state
+            .outputs
+            .iter_mut()
+            .find(|o| o.wl == ctx.proxy)
+            .unwrap();
+        let name = String::from_utf8(name.into_bytes()).expect("invalid output name");
+        eprintln!("Output {}: name = {name:?}", output.reg_name);
+        ctx.state.dbus_server.add_output(output.reg_name, &name);
+        output.name = Some(name);
+    }
+}
